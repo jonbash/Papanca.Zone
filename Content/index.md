b1cf5a1a2028fbefb2aaf3164c0cc3d2f1ceeac2
Jon Bash <jonbash@protonmail.com>
2020-08-07 12:09:56 -0700

    init

---
+++ Content/index.md
@@ -0,0 +1,1 @@
+# Papanca.Zone
---
+++ Sources/PapancaZone/main.swift
@@ -0,0 +1,26 @@
+import Foundation
+import Publish
+import Plot
+
+// This type acts as the configuration for your website.
+struct PapancaZone: Website {
+    enum SectionID: String, WebsiteSectionID {
+        // Add the sections that you want your website to contain here:
+        case posts
+        case glossary
+    }
+
+    struct ItemMetadata: WebsiteItemMetadata {
+        // Add any site-specific metadata that you want to use here.
+    }
+
+    // Update these properties to configure your website:
+    var url = URL(string: "https://papanca.zone")!
+    var name = "Papanca.Zone"
+    var description = "A proliferation of meditation and Dharma stuff"
+    var language: Language { .english }
+    var imagePath: Path? { nil }
+}
+
+// This will generate your website using the built-in Foundation theme:
+try PapancaZone().publish(withTheme: .foundation)
